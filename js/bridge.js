e4f697d1fe37cbb2fef5612800d4104e42573faa
Joakim Löfgren <joakim.lofgren@gmail.com>
2013-01-19 21:29:35 +0100

    Convert facebook id to spotify id.

---
+++ js/bridge.js
@@ -0,0 +1,42 @@
+/**
+ * Permissions needed ['user_actions.music', 'friends_actions.music'];
+ *
+ * Usage:
+ * bridge.fbToSpotify('me', function (user) {
+ * 	   bridge.getTopTracks(user, function (track) {
+ *         console.log(user, track.data.name, track.data.uri);
+ *     });
+ * });
+ *
+ * Replace me with facebook id.
+ */
+var Bridge = function (token) {
+	"use strict";
+
+	var accessToken = token;
+
+	return {
+		fbToSpotify: function (user, callback) {
+			var url = 'https://graph.facebook.com/' + user + '/music.playlists?access_token=' + accessToken;
+	        $.get(url, function (response) {
+	            user = response.data[0].data.playlist.url.split('/')[4];
+	            callback(user);
+	        }, 'json');
+	    },
+
+    	getTopTracks: function (user, callback) {
+	        var toplist = new models.Toplist();
+	        toplist.toplistType = models.TOPLISTTYPE.USER;
+	        toplist.matchType = models.TOPLISTMATCHES.TRACKS;
+	        toplist.userName = user;
+
+	        toplist.observe(models.EVENT.CHANGE, function() {
+	            toplist.results.forEach(function(track) {
+	                callback(track);
+	            });
+	        });
+
+	        toplist.run();
+	    }
+	};
+};
