2962c68fd024b10c3b2902c5d1a0146431a06a25
Dijam <dijam@Dijam-Pro.local>
2013-01-19 21:33:13 +0100

    Playlist

---
+++ js/playlist.js
@@ -0,0 +1,18 @@
+var SPPlayList = {}
+
+SPPlayList.uri = "";
+SPPlayList.tracks = {};
+SPPlayList.name = "";
+
+SPPlayList.init = function(name, tracks) {
+    var sp = getSpotifyApi();
+	var models = sp.require('$api/models');
+	var playlist = new models.Playlist(name);
+	SPPlayList.name = name;
+	playlist.data.collaborative = true;
+
+	tracks.forEach(function(track){
+		playlist.add(models.Track.fromURI(track.uri));	
+	});
+	SPPlayList.tracks = playlist.tracks;
+}
